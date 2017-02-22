2701cab7df2644daa3e522d54148257f2b51e21b
yinyanlv <yanlv.yin@gmail.com>
2017-02-22 11:25:48 +0800

    修改bst

--- src/bugong/BST.java
+++ src/bugong/BST.java
@@ -31,13 +31,10 @@ public class BST {
 
     public void insert(int key, int value) {
 
-
         root = insert(root, key, value);
-
     }
 
     private Node insert(Node node, int key, int value) {
-
 
         if (node == null) {
 
@@ -66,7 +63,6 @@ public class BST {
 
     public boolean contain(int key) {
 
-
         return contain(root, key);
     }
 
@@ -91,7 +87,6 @@ public class BST {
         return search(root, key);
     }
 
-
     private Integer search(Node node, int key) {
 
         if (node == null) return null;
@@ -105,6 +100,92 @@ public class BST {
         } else {
 
             return search(node.left, key);
+        }
+    }
+
+    /**
+     * 前序遍历
+     */
+    public void preorder() {
+
+        if (root == null) return;
+
+        preorder(root);
+    }
+
+    private void preorder(Node node) {
+
+        if (node != null) {
+
+            System.out.println("preorder - current node is: " + node.key);
+
+            preorder(node.left);
+            preorder(node.right);
+        }
+    }
+
+    /**
+     * 中序遍历
+     */
+    public void inorder() {
+
+        if (root == null) return;
+
+        inorder(root);
+    }
+
+    private void inorder(Node node) {
+
+        if (node != null) {
+
+            preorder(node.left);
+
+            System.out.println("inorder - current node is: " + node.key);
+
+            preorder(node.right);
+        }
+    }
+
+    /**
+     * 后序遍历
+     */
+    public void postorder() {
+
+        if (root == null) return;
+
+        postorder(root);
+    }
+
+    private void postorder(Node node) {
+
+        if (node != null) {
+
+            preorder(node.left);
+            preorder(node.right);
+
+            System.out.println("postorder - current node is: " + node.key);
+        }
+    }
+
+    /**
+     * 层序遍历（广度优先遍历）
+     */
+    public void levelOrder() {
+
+        if (root == null) return;
+
+        Queue<Node> queue = new Queue();
+
+        queue.enqueue(root);
+
+        while (!queue.isEmpty()) {
+
+            Node node = queue.dequeue();
+
+            System.out.println("current node is: " + node.key);
+
+            if (node.left != null) queue.enqueue(node.left);
+            if (node.right != null) queue.enqueue(node.right);
         }
     }
 
