411aabcf501592b61b6fa207bd77bfa5ec198505
yinyanlv <yanlv.yin@gmail.com>
2017-06-07 11:00:14 +0800

    add: dense graph

---
+++ src/bugong/DenseGraph.java
@@ -0,0 +1,14 @@
+package bugong;
+
+import java.util.ArrayList;
+
+public class DenseGraph {
+
+    private ArrayList graph;
+
+
+    public static void main(String[] args) {
+
+        System.out.println("dense graph");
+    }
+}
