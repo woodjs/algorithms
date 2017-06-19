f6c9334577da6ee14468d6f0f1970b7e55af0299
yinyanlv <yanlv.yin@gmail.com>
2017-06-19 11:27:04 +0800

    优化图接口

--- src/bugong/Graph.java
+++ src/bugong/Graph.java
@@ -2,9 +2,9 @@ package bugong;
 
 public interface Graph {
 
-    public int getNodeCount();
-    public int getEdgeCount();
-    public void addEdge(int n, int m);
-    public boolean hasEdge(int n, int m);
-    public Iterable<Integer> getAdjacentNodes(int n);
+    int getNodeCount();
+    int getEdgeCount();
+    void addEdge(int n, int m);
+    boolean hasEdge(int n, int m);
+    Iterable<Integer> getAdjacentNodes(int n);
 }
--- src/bugong/WeightGraph.java
+++ src/bugong/WeightGraph.java
@@ -2,9 +2,9 @@ package bugong;
 
 public interface WeightGraph {
 
-    public int getNodeCount();
-    public int getEdgeCount();
-    public void addEdge(Edge e);
-    public boolean hasEdge(int n, int m);
-    public Iterable<Edge> getAdjacentEdges(int n);
+    int getNodeCount();
+    int getEdgeCount();
+    void addEdge(Edge e);
+    boolean hasEdge(int n, int m);
+    Iterable<Edge> getAdjacentEdges(int n);
 }
