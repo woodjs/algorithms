c38f16cade6ff2f5e05715080275922f253c93cb
yinyanlv <yanlv.yin@gmail.com>
2017-06-20 11:08:31 +0800

    修改sort注释

--- src/bugong/Sort.java
+++ src/bugong/Sort.java
@@ -95,7 +95,7 @@ public class Sort {
 
         for (; gap > 0; gap = (int) Math.floor(gap / 3)) {
 
-            for (int i = gap - 1; i < len - gap; i++) {  // 1，防止arr[j]越界
+            for (int i = gap - 1; i < len - gap; i++) {  // gap，防止arr[j]越界
 
                 for (int j = i + gap; j >= gap && arr[j] < arr[j - gap]; j -= gap) {  // gap，防止arr[j - gap]越界
 
